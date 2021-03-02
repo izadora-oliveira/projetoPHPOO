2d0c915ebbb656a39ad5155e4c91f2ab188c5811
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-03-02 18:28:04 -0300

    Create livros.php

---
+++ livros.php
@@ -0,0 +1,47 @@
+<?php
+
+class Livros
+{
+    //atributos
+  public $nome;
+  public $autor;
+  public $lancamento;
+
+  //construtor da classe
+  function __construct($nome_c, $autor_c, $lancamento_c)
+  {
+
+    $this->nome = $nome_c;
+    $this->autor = $autor_c;
+    $this->lancamento = $lancamento_c;
+  }
+
+  // setters
+  public function setNome($nome)
+  {
+    $this->nome = $nome;
+  }
+  public function setAutor($autor)
+  {
+    $this->autor = $autor;
+  }
+  public function setLancamento($lancamento)
+  {
+    $this->lancamento = $lancamento;
+  }
+
+  //getters
+  function getNome()
+  {
+    return $this->nome;
+  }
+  function getAutor()
+  {
+    return $this->autor;
+  }
+  function getLancamento()
+  {
+    return $this->lancamento;
+  }
+  
+}
