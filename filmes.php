78f069f90af2e9506019ae0dc6f568a2a4fe402b
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-03-02 18:28:01 -0300

    Create filmes.php

---
+++ filmes.php
@@ -0,0 +1,45 @@
+<?php
+class Filmes
+{
+  //atributos
+  public $nome;
+  public $lancamento;
+  public $genero;
+
+  //construtor da classe
+  function __construct($nome_c, $lancamento_c, $genero_c)
+  {
+
+    $this->nome = $nome_c;
+    $this->lancamento = $lancamento_c;
+    $this->genero = $genero_c;
+  }
+
+  // setters
+  public function setNome($nome)
+  {
+    $this->nome = $nome;
+  }
+  public function setLancamento($lancamento)
+  {
+    $this->lancamento = $lancamento;
+  }
+  public function setGenero($genero)
+  {
+    $this->genero = $genero;
+  }
+
+  //getters
+  function getNome()
+  {
+    return $this->nome;
+  }
+  function getLancamento()
+  {
+    return $this->lancamento;
+  }
+  function getGenero()
+  {
+    return $this->genero;
+  }
+}
