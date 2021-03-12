1eee8f857b71a26f69077668721c0ef38c81b47c
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-03-12 17:29:09 -0300

    Update entra.php

--- entra.php
+++ entra.php
@@ -14,7 +14,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="entrar.php">Cadastrar</a>
+          <a class="nav-link active" aria-current="page" href="cadastro.php">Cadastrar</a>
         </li>
     </div>
   </div>
