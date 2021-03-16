c0462c88cec4f3c5fd70fafeceb9cf176c0abb7b
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-03-16 15:47:55 -0300

    Update cadastro.php

--- classes/cadastro.php
+++ classes/cadastro.php
@@ -8,10 +8,10 @@ class Cadastro
   public $senha;
 
 	public function __get($atributo) {
-		return $this->$atributo;
+	return $this->$atributo;
 	}
 
 	public function __set($atributo, $valor) {
-		$this->$atributo = $valor;
+	$this->$atributo = $valor;
 	}
 }
