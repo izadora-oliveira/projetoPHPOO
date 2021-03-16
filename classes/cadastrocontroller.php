5ce65dd1a50b709abe2897913fec940584aa2b79
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-03-16 15:47:59 -0300

    Update cadastrocontroller.php

--- classes/cadastrocontroller.php
+++ classes/cadastrocontroller.php
@@ -1,18 +1,21 @@
 <?php
 
-require "../../projetoPHPOO\classes\cadastro.php";
-require "../../projetoPHPOO\classes\cadastroservice.php";
-require "../../projetoPHPOO\classes\conexao.php";
+require "cadastro.php";
+require "cadastroservice.php";
+require "conexao.php";
 
+$n = $_POST['nome'];
+$e = $_POST['email'];
+$s = $_POST['senha'];
 
 $cadastro = new Cadastro();
-$cadastro->__set('nome', $_POST['nome']);
-$cadastro->__set('email', $_POST['email']);
-$cadastro->__set('senha', $_POST['senha']);
+$cadastro->__set('nome', $n);
+$cadastro->__set('email', $e);
+$cadastro->__set('senha', $s);
 
 $conexao = new Conexao();
 
-$cadastroService = new CadastroService($conexao, $nome, $email, $senha);
+$cadastroService = new CadastroService($conexao, $n, $e, $s);
 $cadastroService->inserir();
 
 header('Location:../cadastrar.php?inclusao=1');
