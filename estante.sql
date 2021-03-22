d41033f435efbf47b43625f44f1e4aaa8f391276
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-03-22 15:59:51 -0300

    Create estante.sql

---
+++ estante.sql
@@ -0,0 +1,153 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 22-Mar-2021 às 19:58
+-- Versão do servidor: 10.4.17-MariaDB
+-- versão do PHP: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `estante`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `filmes`
+--
+
+CREATE TABLE `filmes` (
+  `idestrangeira` int(11) NOT NULL,
+  `imagem` mediumblob NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `lancamento` int(10) NOT NULL,
+  `genero` varchar(100) NOT NULL,
+  `lugar` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `livros`
+--
+
+CREATE TABLE `livros` (
+  `idlivro` int(10) NOT NULL,
+  `imagem` mediumblob NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `autor` varchar(100) NOT NULL,
+  `lancamento` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `series`
+--
+
+CREATE TABLE `series` (
+  `idserie` int(10) NOT NULL,
+  `imagem` mediumblob NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `temporada` int(10) NOT NULL,
+  `lancamento` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idusuario` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`idusuario`, `nome`, `email`, `senha`) VALUES
+(1, 'izadora', 'izamartins7@outlook.com', 123456),
+(2, 'joao', 'joao@gmail.com', 123456),
+(3, 'maria', 'maria123@hotmail.com', 123456),
+(4, 'ana', 'ana@gmail.com', 123456);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `filmes`
+--
+ALTER TABLE `filmes`
+  ADD KEY `idestrangeira` (`idestrangeira`);
+
+--
+-- Índices para tabela `livros`
+--
+ALTER TABLE `livros`
+  ADD KEY `idlivro` (`idlivro`);
+
+--
+-- Índices para tabela `series`
+--
+ALTER TABLE `series`
+  ADD KEY `idserie` (`idserie`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idusuario`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `filmes`
+--
+ALTER TABLE `filmes`
+  ADD CONSTRAINT `idestrangeira` FOREIGN KEY (`idestrangeira`) REFERENCES `usuario` (`idusuario`);
+
+--
+-- Limitadores para a tabela `livros`
+--
+ALTER TABLE `livros`
+  ADD CONSTRAINT `idlivro` FOREIGN KEY (`idlivro`) REFERENCES `usuario` (`idusuario`);
+
+--
+-- Limitadores para a tabela `series`
+--
+ALTER TABLE `series`
+  ADD CONSTRAINT `idserie` FOREIGN KEY (`idserie`) REFERENCES `usuario` (`idusuario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
