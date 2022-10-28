8e784d4a35e1f6aa9eaa6f4d160075ff25c74df2
izadora oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2022-10-28 00:33:00 -0300

    ajustes galeira de filmes 28/10

--- index.php
+++ index.php
@@ -1,41 +1,22 @@
-<?php
-require_once("classes/verificalogin.php");
-?>
+ <!DOCTYPE html>
+ <html lang="pt-br">
+ <head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <title>Faster Filmes (FF)</title>
+ </head>
+ <body>
+ <div class="container" style="max-width: 100%; padding: 0;">
+ <?php 
+  require("nav.php");
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <title>Estante</title>
-</head>
-<body>
-<div class="card w-75">
-  <div class="card-body">
-    <h5 class="card-title">Estante Digital</h5>
-<table class="table">
-  <tbody>
-    <tr>
-      <th scope="row"><a href="filmes.php" >Filmes</a></th>
-    </tr>
-    <tr>
-      <th scope="row"><a href="series.php" >Séries</a></th>
-    </tr>
-    <tr>
-      <th scope="row"><a href="livros.php" >Livros</a></th>
-    </tr>
-    <tr>
-      <th scope="row"><a href="musicas.php" >Músicas</a></th>
-    </tr>
-    <tr>
-      <th scope="row"><a href="classes/logout.php" >Sair</a></th>
-    </tr> 
-  </tbody>
-</table>
-  </div>
+  ?>
 </div>
 
-</body>
-</html>
+   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+ </body>
+ </html>
---
+++ nav.php
@@ -0,0 +1,23 @@
+<!-- Image and text -->
+<nav class="navbar navbar-light bg-dark justify-content-between">
+  <a class="navbar-brand" style="color: white; font-size: xx-large;" href="index.php">
+    <img src="imagens/logo_filme.png" width="50" height="50" class="d-inline-block align-top" alt="">
+    Faster Filmes (FF)
+  </a>
+ <div style="display: flex;">
+ <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Digite o Nome do Filme" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+  <li class="nav-item dropdown">
+        <a class="nav-link dropdown" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span class="navbar-toggler-icon"></span>
+        </a>
+        <div class="dropdown-menu" style="right: 0; left: inherit !important;" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Gênero</a>
+          <a class="dropdown-item" href="#">Lançamentos</a>
+          <a class="dropdown-item" href="#">Nacionais</a>
+        </div>
+      </li>
+ </div>
+</nav>
